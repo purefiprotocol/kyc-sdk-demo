d238ea96dc1aef19402091404eba62be7355c624
Mikhail Radchenko <misha-radchenko@yandex.ru>
2024-05-08 11:04:32 +0300

    feat: migrate to viem chains

--- src/config/chains.js
+++ src/config/chains.js
@@ -1,29 +1,4 @@
-import { sepolia, bscTestnet } from 'wagmi/chains';
-
-const arbitrumSepolia = {
-  id: 421614,
-  name: 'Arbitrum Sepolia',
-  network: 'arbitrumSepolia',
-  nativeCurrency: {
-    name: 'ETH',
-    symbol: 'ETH',
-    decimals: 18,
-  },
-  rpcUrls: {
-    default: {
-      http: ['https://sepolia-rollup.arbitrum.io/rpc'],
-    },
-    public: {
-      http: ['https://sepolia-rollup.arbitrum.io/rpc'],
-    },
-  },
-  blockExplorers: {
-    default: {
-      name: 'Arbiscan',
-      url: 'https://sepolia.arbiscan.io',
-    },
-  },
-};
+import { sepolia, arbitrumSepolia, bscTestnet } from 'viem/chains';
 
 bscTestnet.name = 'BSC Testnet';
 
